b78c6586ab68248d85d296aa5f55ebcf5ee0770b
Juan Fernandez <jcfernan@live.com>
2020-04-09 01:36:23 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,31 +17,31 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  dog_brain= ["food", "h2o", "toy"]
+  dog_brain= ["food", "h2o", "toy", "arrays!"]
  deputy_dog= dog_brain.pop
 end
 
 def remove_element_from_start_of_array(array)
-  truck= ["ford", "chevy", "dodge"]
-  tonka= shift.truck
+  truck= ["wow", "ford", "chevy", "dodge"]
+  tonka= truck.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  superhero= ["superman", "batman", "flash"]
-  superhero[1]
+  superhero= ["superman", "batman", "am"]
+  superhero[2]
 end
 
 def retrieve_first_element_from_array(array)
-  fruit= ["apple", "orange", "pina"]
+  fruit= ["wow", "apple", "orange", "pina"]
   fruit[0]
 end
 
 def retrieve_last_element_from_array(array)
-  home= ["tucson", "phoenix", "el paso"]
+  home= ["tucson", "phoenix", "el paso", "arrays!"]
   home[-1]
 end
 
 def update_element_from_index(array, index_number, element)
 dino=["steg", "cheeta", "trex"]
-dino[1]= "bronc"
+dino[1]= "totally"
 end
